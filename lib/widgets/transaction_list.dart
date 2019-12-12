a0bcda7b46d7fbdb29e8fdaac33703f9b1942b2e
wenksi <silvan.wenk@gmail.com>
2019-12-12 22:21:08 +0100

    Replace Scrollable Column with ListView and use Builder pattern

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,46 +11,45 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 350,
-      child: SingleChildScrollView(
-        child: Column(
-          children: this.transactions.map((tx) {
-            return Card(
-              color: Colors.grey[100],
-              elevation: 3,
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    child: Text(
-                      '\$${tx.amount}',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple),
-                    ),
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.purple, width: 2)),
-                    padding: EdgeInsets.all(10),
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+            color: Colors.grey[100],
+            elevation: 3,
+            child: Row(
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    '\$${transactions[index].amount}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        tx.title,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 15),
-                      ),
-                      Text(
-                        DateFormat.yMMMd().format(tx.timestamp),
-                        style: TextStyle(color: Colors.grey[700]),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            );
-          }).toList(),
-        ),
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.purple, width: 2)),
+                  padding: EdgeInsets.all(10),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transactions[index].title,
+                      style:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                    ),
+                    Text(
+                      DateFormat.yMMMd().format(transactions[index].timestamp),
+                      style: TextStyle(color: Colors.grey[700]),
+                    )
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+        itemCount: this.transactions.length,
       ),
     );
   }
