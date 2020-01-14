f01d69d426ee6a27e0db1fcd6c0049bfd7361615
wenksi <silvan.wenk@gmail.com>
2020-01-14 20:25:57 +0100

    Polishing

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,7 +11,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 350,
+      height: 430,
       child: this.transactions.isEmpty
           ? Column(
               children: <Widget>[
