d253fe74af06e78635a079a12af93837cb4239f6
wenksi <silvan.wenk@gmail.com>
2020-01-28 15:57:56 +0100

    Use WidgetsBindungObserver pattern to view AppState

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   final List<Transaction> _userTransactions = [
     Transaction(
         id: 't1', amount: 69.99, timestamp: DateTime.now(), title: "New Shoes"),
@@ -63,6 +63,23 @@ class _MyHomePageState extends State<MyHomePage> {
   ];
 
   bool _showChart = false;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print(state);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
 
   List<Transaction> get _recentTransaction {
     return this._userTransactions.where((t) {
